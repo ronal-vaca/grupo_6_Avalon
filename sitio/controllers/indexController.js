eaf52835628c74a0a38ff5d915859423a1a03c3c
Emiliano Rivarola <emi_rivarola@hotmail.com>
2020-08-23 20:13:17 -0300

    agrego json y imagenes con datos illustrativos en el home

--- sitio/controllers/indexController.js
+++ sitio/controllers/indexController.js
@@ -1,9 +1,21 @@
-const fs = require('fs');
-const path = require('path')
+let dbProducto = require('../data/database')
+
 module.exports={
     home:function(req, res) {    //siempre en ese orden req, res, next con sus respectivos espacios sino da error nose porqe by:emi
-        res.render('index',{
-          title: "Avalon"
-        });
+      let ofertas = dbProducto.filter(producto => {
+         return producto.categoria == "oferta"
+      })
+      let masVendido = dbProducto.filter(producto => {
+        return producto.categoria == "mas-vendido"
+      })
+      let mejorProducto = dbProducto.filter(producto => {
+      return producto.categoria == "mejor-producto"
+      })
+      res.render('index',{
+        title: "Avalon",
+        ofertas: ofertas,
+        masVendido: masVendido,
+        mejorProducto: mejorProducto
+      });
     }
 }
