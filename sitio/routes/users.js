cd9bf6bb3555a59cbd790640f19882409e00781b
Emiliano Rivarola <emi_rivarola@hotmail.com>
2020-09-14 20:15:10 -0300

    comente cosas

--- sitio/routes/users.js
+++ sitio/routes/users.js
@@ -1,15 +1,20 @@
+//----------MODULOS-----------
 var express = require('express');
 var router = express.Router();
+//-------CONTROLADORES--------
 const controller = require('../controllers/usersController');
+//-------VALIDACIONES---------
 let loginValidator = require('../validators/loginValidator');
 
-
+//-------MIDDLEWARES------
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-router.get('/iniciarSesion', controller.iniciarSesion);
-router.post('/iniciarSesion', loginValidator, controller.processLogin);
+
+//-------RUTAS------------
+router.get('/iniciarSesion', controller.iniciarSesion);                  //vista del login
+router.post('/iniciarSesion', loginValidator, controller.processLogin);  //validacion del login 
 
 router.get('/registro', controller.registro);
 router.post('/registro', controller.agregoUsuario);
